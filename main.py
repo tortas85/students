f5caad39583f21fffff310aabf5fef7ce4710d78
tortas85 <tortas11185@gmail.com>
2024-10-20 17:48:47 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+grades = [[5, 3, 3, 5, 4], [2, 2, 2, 3], [4, 5, 5, 2], [4, 4, 3], [5, 5, 5, 4, 5]]
+students = {'Johnny':sum(grades[2])/len(grades[2]), 'Bilbo':sum(grades[1])/len(grades[1]),
+            'Steve':sum(grades[4])/len(grades[4]), 'Khendrik':sum(grades[3])/len(grades[3]) ,
+            'Aaron':sum(grades[0])/len(grades[0])}
+print(students)
+
+
+
+
+
